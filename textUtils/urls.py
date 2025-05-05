9518f267b9887f7eaa61690295e77616e4211326
Fathima Riswana <fathimariswana024@gmail.com>
2025-05-05 07:52:51 +0400

    about page added

--- textUtils/urls.py
+++ textUtils/urls.py
@@ -21,5 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='home'),
-    path('analyse', views.analyse, name='analyse')
+    path('analyse', views.analyse, name='analyse'),
+    path('about/', views.about, name="about")
 ]
--- textUtils/views.py
+++ textUtils/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+def about(request):
+    return render(request, 'about.html')
 def analyse(request):
     if request.method=="GET":
         text = request.GET.get('enteredtext')
